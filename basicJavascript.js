665389c2e91334ee61fd791e7d1f98dea9f636d8
propscode <mrn00@hotmail.com>
2023-04-12 10:39:51 +0200

    I format the code and comments to make it more readable.

--- basicJavascript.js
+++ basicJavascript.js
@@ -44,54 +44,71 @@ var titleCaseOver;
 studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
+
 //introducing the difference between var and let. (let can not be more times declared, you can not overridden: important ES6)
 let catName = "Oliver";
 let catSound = "Meow!";
+
 //introduce the difference between let and const. With Const you can not reassigning the value. The console will display an error due to reassigning the value, const is READ-ONLY and can not change
 // It is common for developers to use uppercase variable identifiers for immutable values and lowercase or camelCase for mutable values (objects and arrays).
 const FCC = "freeCodeCamp"; 
 let fact = "is cool!"; 
 fact = "is awesome!";
 console.log(FCC, fact); 
+
 // Add Two Numbers with JavaScript
 const sum = 10 + 10;
+
 //Subtract One Number from Another with JavaScript
 const difference = 45 - 33;
+
 //Multiply Two Numbers with JavaScript
 const product = 8 * 10;
+
 //Divide One Number by Another with JavaScript
 const quotient = 66 / 33;
+
 //Increment a Number with JavaScript
 let myVarA = 87;
 myVarA++;
+
 //Decrement a Number with JavaScript
 let myVarB = 11;
 myVarB--;
+
 //Create Decimal Numbers with JavaScript (float numbers)
 const ourDecimal = 5.7;
 const myDecimal= 5.7;
+
 //Multiply Two Decimals with JavaScript
 const productDecimal = 2.0 * 2.5;
+
 //Divide One Decimal by Another with JavaScript
 const quotientDecimal = 4.4 / 2.0;
+
 //Finding a Remainder in JavaScript % (Note: The remainder operator is sometimes incorrectly referred to as the modulus operator. It is very similar to modulus, but does not work properly with negative numbers.)
 const remainder = 11%3
+
 //Compound Assignment With Augmented Addition: += Operator
 let g = 3;
 let h = 17;
 let j = 12;
+
 //Use += operator
 g += 12;
 h += 9;
 j += 7;
+
 //Compound Assignment With Augmented Subtraction: -= Operator
 g -= 6;
 h -= 15;
 j -= 1;
+
 //Compound Assignment With Augmented Multiplication: *= Operator
 g *= 5;
 h *= 3;
 j *= 10;
+
 //Compound Assignment With Augmented Division: /=Operator
 g /= 12;
 h /= 4;
