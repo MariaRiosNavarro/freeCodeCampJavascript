98ca96bba1c8cd494c3bdf6213809b067bd25124
propscode <mrn00@hotmail.com>
2023-04-12 11:48:57 +0200

    Escaping Literal Quotes in Strings: with \ beforr " or '.

--- basicJavascript.js
+++ basicJavascript.js
@@ -116,3 +116,4 @@ j /= 11;
 
 // Escaping Literal Quotes in Strings: with \ beforr " or '. In JavaScript, you can escape a quote from considering it as an end of string quote by placing a backslash (\) in front of the quote.
 const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
