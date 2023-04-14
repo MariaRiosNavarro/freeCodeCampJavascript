657f7883f19c8753751309ad7db5576dd3b00d2e
propscode <mrn00@hotmail.com>
2023-04-14 12:16:02 +0200

    Understanding Boolean Values

--- basicJavascript.js
+++ basicJavascript.js
@@ -430,5 +430,21 @@ console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
 
+// Understanding Boolean Values
+// Another data type is the Boolean. Booleans may only be one of two values: true or false. Note: Boolean values are never written with quotes. The strings "true" and "false" are not Boolean
+
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
+
+
+
+
+
+
 
 
