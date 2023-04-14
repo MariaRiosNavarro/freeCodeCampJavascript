cca954a7ac3126bfe0817eebe2a8b1467a017d1b
propscode <mrn00@hotmail.com>
2023-04-14 12:37:24 +0200

    If Statements.

--- basicJavascript.js
+++ basicJavascript.js
@@ -354,14 +354,14 @@ function fun2() {
 // the use of a variable that has been defined in a function, outside this function (e.g. in a console.log) will give an error. Exercise
 
 function myLocalScope() {
-    // ----Only change code below this line
+    // Only change code below this line
     const myVar="hola";
     console.log('inside myLocalScope', myVar);
   }
   myLocalScope();
   
-  // ----Run and check the console
-  // ----myVar is not defined outside of myLocalScope
+  // Run and check the console
+  //myVar is not defined outside of myLocalScope
 
   //console.log('outside myLocalScope', myVar); --->ERROR IN THE CONSOLE (i change it to comment)
 
@@ -448,6 +448,36 @@ function welcomeToBooleans() {
 
 // ------Use Conditional Logic with If Statements. if statements are used to make decisions in code.The keyword if tells JavaScript to execute the code in the curly braces under certain conditions, defined in the parentheses. These conditions are known as Boolean conditions and they may only be true or false.
 // When the condition evaluates to true, the program executes the statement inside the curly braces. When the Boolean condition evaluates to false, the statement inside the curly braces will not execute.
+
+// Pseudocode
+
+// if (condition is true) {
+//   statement is executed
+// }
+
+// ANd example:
+
+// function test (myCondition) {
+//   if (myCondition) {
+//     return "It was true";
+//   }
+//   return "It was false";
+// }
+
+// test(true);
+// test(false);
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+  if(wasThatTrue){
+    return "Yes, that was true";
+  }
+    return "No, that was false";
+
+  // Only change code above this line
+
+}
+
 
 
 
