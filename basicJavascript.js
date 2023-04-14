6629a59ef5cfbd36dbe3f6b5a78426ab14eab9cc
propscode <mrn00@hotmail.com>
2023-04-14 19:26:56 +0200

    Comparison with the Less Than Operator   <

--- basicJavascript.js
+++ basicJavascript.js
@@ -648,3 +648,21 @@ function testGreaterOrEqual(val) {
 testGreaterOrEqual(10);
 console.log(testGreaterOrEqual(10));
 
+// ---------Comparison with the Less Than Operator   <
+// The less than operator (<) compares the values of two numbers. If the number to the left is less than the number to the right, it returns true. Otherwise, it returns false. Like the equality operator, the less than operator converts data types while comparing.
+
+function testLessThan(val) {
+  if (val<25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val<55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+console.log(testLessThan(10));
+
