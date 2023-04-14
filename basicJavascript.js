cdae8024799df6d28dea788a2e7f35555a3ac34d
propscode <mrn00@hotmail.com>
2023-04-14 18:28:27 +0200

    Practice comparing different values & use of typeof

--- basicJavascript.js
+++ basicJavascript.js
@@ -524,7 +524,37 @@ testStrict(10);
 console.log(testStrict(10));
 
 
+// ----Practice comparing different values & use of typeof
+// In the last two challenges, we learned about the equality operator (==) and the strict equality operator (===). Let's do a quick review and practice using these operators some more.
+// If the values being compared are not of the same type, the equality operator will perform a type conversion, and then evaluate the values. However, the strict equality operator will compare both the data type and value as-is, without converting one type to the other.
+// 3 == '3' returns true because JavaScript performs type conversion from string to number. 3 === '3' returns false because the types are different and type conversion is not performed.
+// Note: In JavaScript, you can determine the type of a variable or a value with the typeof operator, as follows:
 
+// typeof 3 returns the string number, and typeof '3' returns the string string.
+
+function compareEquality(a, b) {
+  if (a === b) { // Change this line to do strictly equality operator
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
+console.log(compareEquality(10, "10"))
+
+//and terminal say Not Equal
+
+function compareEqualityB(c, d) {
+  if (c == d) { // Change this line to do equality operator
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEqualityB(10, "10");
+console.log(compareEqualityB(10, "10"));
+
+//and terminal say Equal
 
 
 
