03585ff5569362eb593e8c424721387fc3375834
propscode <mrn00@hotmail.com>
2023-04-15 12:50:31 +0200

    Logical Order in If Else Statements

--- basicJavascript.js
+++ basicJavascript.js
@@ -811,7 +811,67 @@ console.log(testElseIf(2));
 console.log(testElseIf(8));
 console.log(testElseIf(10));
 
+// -------Logical Order in If Else Statements 
+// Order is important in if, else if statements.
+
+// The function is executed from top to bottom so you will want to be careful of what statement comes first.
+
+// Take these two functions as an example.
+
+// Here's the first:
+
+// function foo(x) {
+//   if (x < 1) {
+//     return "Less than one";
+//   } else if (x < 2) {
+//     return "Less than two";
+//   } else {
+//     return "Greater than or equal to two";
+//   }
+// }
+// And the second just switches the order of the statements:
+
+// function bar(x) {
+//   if (x < 2) {
+//     return "Less than two";
+//   } else if (x < 1) {
+//     return "Less than one";
+//   } else {
+//     return "Greater than or equal to two";
+//   }
+// }
+// While these two functions look nearly identical if we pass a number to both we get different outputs.
+
+// foo(0)
+// bar(0)
+// foo(0) will return the string Less than one, and bar(0) will return the string Less than two.
 
 
+function orderMyLogicA(val) {
+  if (val < 10) {
+    return "Less than 10";
+  } else if (val < 5) {
+    return "Less than 5";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogicA(7);
+console.log(orderMyLogicA(7));
+console.log(orderMyLogicA(4));
 
 
+function orderMyLogicB(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else  if (val < 10) {
+    return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+      }
+}
+
+orderMyLogicB(7);
+console.log(orderMyLogicB(7));
+console.log(orderMyLogicB(4));
