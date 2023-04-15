d9823daee362dd3f7c6f3b09b97ce80c5d68dc6f
propscode <mrn00@hotmail.com>
2023-04-15 14:49:22 +0200

    Multiple Identical Options in Switch StatementsMultiple Identical Options in Switch Statements

--- basicJavascript.js
+++ basicJavascript.js
@@ -1064,3 +1064,51 @@ switchOfStuff("c"); // console say cat
 switchOfStuff(a); //console say stuff, becouse case values are tested with strict equality (===), also a ist no the same to "a"
 
 
+// ----Multiple Identical Options in Switch Statements 
+// If the break statement is omitted from a switch statement's case, the following case statement(s) are executed until a break is encountered. If you have multiple inputs with the same output, you can represent them in a switch statement like this:
+
+// let result = "";
+// switch (val) {
+//   case 1:
+//   case 2:
+//   case 3:
+//     result = "1, 2, or 3";
+//     break;
+//   case 4:
+//     result = "4 alone";
+// }
+// Cases for 1, 2, and 3 will all produce the same result.
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+    switch(val) {
+      case 1:
+      case 2:
+      case 3:
+        answer="Low";
+        console.log(answer);
+        break;
+      case 4:
+      case 5:
+      case 6:
+        answer="Mid";
+        console.log(answer);
+        break;
+      case 7:
+      case 8:
+      case 9:
+        answer="High"
+        console.log(answer);
+        break;
+    }
+
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+sequentialSizes(5);
+sequentialSizes(7);
