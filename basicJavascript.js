6add53479e2f31b338f27912ba7b0716fe37f690
propscode <mrn00@hotmail.com>
2023-04-15 13:40:27 +0200

    Selecting from Many Options with Switch Statements switch(n){case"a": ;break; case"b": ;break;}

--- basicJavascript.js
+++ basicJavascript.js
@@ -967,3 +967,18 @@ console.log(golfScore(4, 5));
 console.log(golfScore(4, 6));
 console.log(golfScore(4, 7));
 console.log(golfScore(4, 9));
+
+// ----------Selecting from Many Options with Switch Statements switch(n){case"a": ;break; case"b": ;break;}
+// If you need to match one value against many options, you can use a switch statement. A switch statement compares the value to the case statements which define various possible values. Any valid JavaScript statements can be executed inside a case block and will run from the first matched case value until a break is encountered.
+
+// Here is an example of a switch statement:
+
+// switch (fruit) {
+//   case "apple":
+//     console.log("The fruit is an apple");
+//     break;
+//   case "orange":
+//     console.log("The fruit is an orange");
+//     break;
+// }
+// case values are tested with strict equality (===). The break tells JavaScript to stop executing statements. If the break is omitted, the next statement will be executed.
