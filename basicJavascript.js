8bd493fa752404a71dea23dfb10e6eabfd10abad
propscode <mrn00@hotmail.com>
2023-04-16 14:07:08 +0200

    Iterate Through an Array with a For Loop

--- basicJavascript.js
+++ basicJavascript.js
@@ -1926,3 +1926,22 @@ for(let l=9; l>0; l-=2){
   myArrayO.push(l);
 }
 console.log(myArrayO);
+
+
+// --------------Iterate Through an Array with a For Loop
+// A common task in JavaScript is to iterate through the contents of an array. One way to do that is with a for loop. This code will output each element of the array arr to the console:
+
+// const arr = [10, 9, 8, 7, 6];
+
+// for (let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]);
+// }
+// Remember that arrays have zero-based indexing, which means the last index of the array is length - 1. Our condition for this loop is i < arr.length, which stops the loop when i is equal to length. In this case the last iteration is i === 4 i.e. when i becomes equal to arr.length - 1 and outputs 6 to the console. Then i increases to 5, and the loop terminates because i < arr.length is false.
+
+const myArrA = [2, 3, 4, 5, 6];
+let totalA= 0 ;
+for (let i=0; i < myArrA.length; i++){
+   totalA = totalA + myArrA[i];
+}
+console.log(totalA);
+
