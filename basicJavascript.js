6bb37f76148c3974b517a090752ab0a84bbcd30d
propscode <mrn00@hotmail.com>
2023-04-16 11:41:42 +0200

    Accessing Nested Objects

--- basicJavascript.js
+++ basicJavascript.js
@@ -1608,3 +1608,41 @@ console.log(myObj.hasOwnProperty("middle"));
   console.log(myMusic[0].formats[1]);
   console.log(myMusic[1].title);
 
+// -----------  Accessing Nested Objects
+// The sub-properties of objects can be accessed by chaining together the dot or bracket notation.
+
+// Here is a nested object:
+
+// const ourStorage = {
+//   "desk": {
+//     "drawer": "stapler"
+//   },
+//   "cabinet": {
+//     "top drawer": { 
+//       "folder1": "a file",
+//       "folder2": "secrets"
+//     },
+//     "bottom drawer": "soda"
+//   }
+// };
+
+// ourStorage.cabinet["top drawer"].folder2;
+// ourStorage.desk.drawer;
+// ourStorage.cabinet["top drawer"].folder2 would be the string secrets, and ourStorage.desk.drawer would be the string stapler
+
+
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+console.log(gloveBoxContents);
+
